b4b2803a026f372c70d202295303a83cf0c4f061
nelso0on <kretaor.kretaor@gmail.com>
2021-01-08 01:27:34 +0100

    First Version Of Site

---
+++ assets/scripts/wavify.js
@@ -0,0 +1,14 @@
+$("#wave").wavify({
+  height: 40,
+  bones: 3,
+  amplitude: 40,
+  color: "#6633ff",
+  speed: 0.25,
+});
+$("#wave2").wavify({
+  height: 30,
+  bones: 3,
+  amplitude: 40,
+  color: "#6633ff73",
+  speed: 0.25,
+});
